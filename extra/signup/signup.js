c691d14389d2265493bf41bdb84b382ae360e6ab
Trying to code <rea936405@gmail.com>
2026-04-07 21:34:48 -0700

    checks and displays validity of inputs (at a basic level))
    note that chrome has a strange error when it comes to the input field on my computer, if the user begins typing it will say the password is valid even with too few characters

--- extra/signup/signup.js
+++ extra/signup/signup.js
@@ -1,2 +1,10 @@
 //this connects the signup page to firebase api
 
+let passwordField = document.body.querySelector("[data-js-tag='password-field']");
+let usernameField = document.querySelector("[data-js-tag='username-field']");
+
+passwordField.addEventListener("input", (input) => {
+    passwordField.checkValidity();
+    console.log(passwordField.checkValidity);
+    console.log(input);
+})
